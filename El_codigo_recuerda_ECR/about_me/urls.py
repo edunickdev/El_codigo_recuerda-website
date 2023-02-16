3f04d398916ba554fa1bcfb6ca1f6b5647f369ed
edunickdev <edunsarmiento@soy.sena.edu.co>
2023-02-16 18:19:25 -0500

    adding official images and a few of preliminar styles

--- El_codigo_recuerda_ECR/about_me/urls.py
+++ El_codigo_recuerda_ECR/about_me/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('create/', views.Create, name='create'),
-    path('read/', views.Read, name='read'),
+    path('', views.Read, name='read'),
     path('update/', views.Update, name='update'),
     path('delete/', views.Delete, name='delete')
 ]
